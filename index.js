ebe7a73e4b1bcbb98035571bdd41954232b72665
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-11-21 18:23:56 -0800

    added more to the mainMenu

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ function mainMenu() {
         type: 'list',
         name: 'action',
         message: 'What would you like to do?',
-        choices: ['Add Employee', 'Delete Employee', 'Exit'],
+        choices: [
+          'Add Employee',
+          'Delete Employee',
+          'Update Employee Role',
+          'View All Roles',
+          'Add Role',
+          'View All Departments',
+          'Add Department',
+          'Exit',
+        ],
       },
     ])
     .then((answer) => {
@@ -27,6 +36,21 @@ function mainMenu() {
           break;
         case 'Delete Employee':
           deleteEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'View All Roles':
+          viewAllRoles();
+          break;
+        case 'Add Role':
+          addRole();
+          break;
+        case 'View All Departments':
+          viewAllDepartments();
+          break;
+        case 'Add Department':
+          addDepartment();
           break;
         case 'Exit':
           connection.end();
@@ -117,8 +141,78 @@ function addEmployee() {
 
 // Function to delete an employee
 function deleteEmployee() {
-  // Implement code to delete an employee
-  console.log('Delete Employee functionality will be implemented here.');
-  // After deleting the employee, return to the main menu
+  connection.query('SELECT id, first_name, last_name FROM employee', (err, employees) => {
+    if (err) {
+      console.error('Error querying database for employees:', err);
+      return;
+    }
+
+    const employeeChoices = employees.map((employee) => ({
+      name: `${employee.first_name} ${employee.last_name}`,
+      value: employee.id,
+    }));
+
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'employeeId',
+          message: 'Select the employee to delete:',
+          choices: employeeChoices,
+        },
+      ])
+      .then((answers) => {
+        const { employeeId } = answers;
+        const sql = 'DELETE FROM employee WHERE id = ?';
+
+        connection.query(sql, [employeeId], (err, results) => {
+          if (err) {
+            console.error('Error deleting employee:', err);
+          } else {
+            console.log('Employee deleted successfully!');
+          }
+          mainMenu(); // Return to the main menu
+        });
+      });
+  });
+}
+
+// Function to update an employee's role
+function updateEmployeeRole() {
+  // Implement code to update employee role
+  console.log('Update Employee Role functionality will be implemented here.');
+  // After updating the employee role, return to the main menu
+  mainMenu();
+}
+
+// Function to view all roles
+function viewAllRoles() {
+  // Implement code to view all roles
+  console.log('View All Roles functionality will be implemented here.');
+  // After viewing all roles, return to the main menu
+  mainMenu();
+}
+
+// Function to add a role
+function addRole() {
+  // Implement code to add a role
+  console.log('Add Role functionality will be implemented here.');
+  // After adding a role, return to the main menu
+  mainMenu();
+}
+
+// Function to view all departments
+function viewAllDepartments() {
+  // Implement code to view all departments
+  console.log('View All Departments functionality will be implemented here.');
+  // After viewing all departments, return to the main menu
+  mainMenu();
+}
+
+// Function to add a department
+function addDepartment() {
+  // Implement code to add a department
+  console.log('Add Department functionality will be implemented here.');
+  // After adding a department, return to the main menu
   mainMenu();
 }
